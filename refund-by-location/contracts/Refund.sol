553adb6e0ee8c8a63209d9ccce81a1e99ab48003
JANEROSE <janerosenyams@gmail.com>
2022-10-30 14:07:19 +0300

    added refund smart contract

--- refund-by-location/contracts/Refund.sol
+++ refund-by-location/contracts/Refund.sol
@@ -3,45 +3,52 @@ pragma solidity 0.8.17;
 
 //THE SMART CONTRACT
 contract Refund {
-    
-    // GLOBAL VARIABLES
+    // STATE VARIABLES
     address public creator; // account address from Metamask
 
-    int public latOrigin; // set latitude
+    int256 public prevLat; // set latitude
 
-    int public longOrigin; // set longitude
+    int256 public prevLong; // set longitude
 
-    uint public timestamp; // timestamp at which location has been received
-    
-    int public longitudeGPS; // Longitude from GPS sensor
-    
-    int public latitudeGPS; // Latitude from GPS sensor
-    
-    uint public deviceAccount; // used to identify device
+    uint256 public timestamp; // timestamp at which location has been received
 
-    bool public  ComplianceStatus; // smart contract validity
+    int256 public longitudeGPS; // Longitude from GPS sensor
 
-    string public  ComplianceDetail; // smart contract validity message
+    int256 public latitudeGPS; // Latitude from GPS sensor
 
-    address[] public employeeDevices; // array of employee devices
+    uint256 public deviceAccount; // used to identify device
+
+    bool public ComplianceStatus; // smart contract validity
+
+    string public ComplianceDetail; // smart contract validity message
+
+    // array of employee device addresses - stored on blockchain
+    address[] public employeeDevices;
+
+    int256 public GPSRange; // location range
+
+    int256 public TimeLimit; // check location after every
 
     struct EmployeeDevice {
         // track employee device details
-        int lonGPS; // GPS longitude
-        int latGPS; // GPS latitude
-        int lonSet; // set longitude
-        int latSet; // set latitude
-        uint time; // timestamp
-        uint deviceID; // device account
+        int256 lonGPS; // GPS longitude
+        int256 latGPS; // GPS latitude
+        int256 lonPrev; // set longitude
+        int256 latPrev; // set latitude
+        uint256 timePrev; // previous timestamp
+        uint256 timeCurr; // current timestamp
+        uint256 deviceID; // device account
     }
 
-    // address is the “key” to access Employee Details
+    // using address as “key” to access EmployeeDevice Struct
     mapping(address => EmployeeDevice) public employeeInfo;
 
-    constructor() {
-        // set up owner of the contract as owner of account 
+    constructor(int256 timeLimit, int256 gpsRange) {
+        // set up owner of the contract as owner of account
         creator = msg.sender;
         ComplianceStatus = true;
+        TimeLimit = timeLimit; // how often should employee location be checked
+        GPSRange = gpsRange; // how far should the employee be from previous location
     }
 
     function kill(address ownerAddress) public {
@@ -49,86 +56,83 @@ contract Refund {
         // only account owner (contract owner) can kill it
         // any Ether remaining in the contract will be sent to owner's account
 
-        require(ownerAddress == msg.sender, "only the owner can call this");
+        require(
+            ownerAddress == msg.sender,
+            "Only contract owner can call this"
+        );
         selfdestruct(payable(ownerAddress));
     }
 
-    // reading coordinates
-    function readCoordinates(address player) public view returns (bool) {
-        // checks that the user has not played already
-        // a person plays once per game
-
-        // does not require gas to run because it’s returning an already existing value from the blockchain i.e. it’s reading a value.
-        for (uint256 i = 0; i < players.length; i++) {
-            if (players[i] == player) return true;
-        }
-        return false;
+    function getEmployeeDeviceID(address empAddress)
+        public
+        view
+        returns (int devID)
+    {
+        // using Ethereum address to fetch employee ID - the mapping
+        EmployeeDevice storage empDevice = employeeInfo[empAddress];
+        return (empDevice.deviceID);
     }
 
-    // sending coordinates function
-    function sendCoordinates(uint256 numberSelected) public payable {
-        // payable is a modifier used to indicate that
-        // this function can receive ether when you executed
-
-        // verify that the player hasn’t placed a bet
-        require(!checkPlayerExists(msg.sender));
-        // verify that the number is in range
-        require(numberSelected >= 1 && numberSelected <= 10);
-        // verify that the bet placed is greater than or equal to the minimum bet.
-        require(msg.value >= minimumBet);
-
-        // msg.sender = address
-        // msg.value is the amount of ether that is paid when executing this payable function.
-        playerInfo[msg.sender].amountBet = msg.value;
-        playerInfo[msg.sender].numberSelected = numberSelected;
-        numberOfBets++; // increase number of placed bets
-        players.push(msg.sender); // adds a new player to the end of the array
-        totalBet += msg.value; // increment the total amount bet
-
-        if (numberOfBets >= maxAmountOfBets) generateNumberWinner();
+    function addAddress(address empAddress) public {
+        //add account address to the array
+        employeeDevices.push(empAddress);
     }
 
-    // Generates a number between 1 and 10 that will be the winner
-    // Solidity does not generate randomness
-    // Here we at least try to generate some random number as winner
-    function generateNumberWinner() public {
-        uint256 numberGenerated = (block.number % 10) + 1; // This isn't secure
+    function readCoordinates(address empAddress)
+        public
+        view
+        returns (int256[] memory)
+    {
+        // fetching employee coordinates from blockchain
+        // NO gas required to run because it’s reading a value
 
-        distributePrizes(numberGenerated);
+        // memory array not stored on blockchain
+        EmployeeDevice calldata employee = employeeInfo[empAddress];
+        int256[4] locations;
+        locations.push(employee.latGPS);
+        locations.push(employee.lonGPS);
+        locations.push(employee.latPrev);
+        locations.push(employee.lonPrev);
+        return locations;
     }
 
-    // Sends the corresponding ether to each winner depending on the total bets
-    function distributePrizes(uint256 numberWinner) public {
-        address payable[100] memory winners; // create a temporary in memory array with fixed size
+    function sendCoordinates(
+        int256 latP,
+        int256 longP,
+        int256 latG,
+        int256 longG,
+        int256 devID,
+        int256 timeStamp
+    ) public {
+        // sending coordinates function
+        // current time at which location is gotten
+        employeeDevices.push(
+            EmployeeDevice(longG, latG, longP, latP, timeStamp, devID)
+        );
+    }
+
+    function calculateTimeDiff(uint timePrev, uint timeCurr) public {
+        //
+    }
+    function determineCompliance() public {
+        // check if contract is in compliance
         
-        uint256 count = 0; // count number of winners
-        for (uint256 i = 0; i < players.length; i++) {
-            // loop through total players
-            address playerAddress = players[i];
-            if (playerInfo[playerAddress].numberSelected == numberWinner) {
-                // add to winners list
-                winners[count] = payable(playerAddress);
-                count++;
-            }
-            // clear players list so the game can restart
-            delete playerInfo[playerAddress];
-        }
-        // also clear the players array - should reset the array length to zero
-        delete players;
-        
-        // Calculate how much each winner gets : total bet amount / number of winners
-        // array.length is read-only
-        uint256 winnerEtherAmount = totalBet / winners.length; 
-        for (uint256 j = 0; j < count; j++) {
-            if (winners[j] != address(0))
-                // Check that the address in this fixed array is not empty
-                // send the corresponding amount of ether for each winner 
-                winners[j].transfer(winnerEtherAmount);
+    }
+
+    function getContractStatus() public view {
+        // check contract compliance
+        if (ComplianceStatus) {
+            // smart contract is in compliance
+            ComplianceDetail = "The smart contract is in compliance";
+            return ComplianceDetail;
+        } else {
+            ComplianceDetail = "The smart contract is in out of compliance";
+            return ComplianceDetail;  
         }
     }
 
     // increases the treasury of this contract
     // executed when ether is sent to the contract
     // save the ether sent to the contract instead of rejecting it
-   receive() external payable {}
+    receive() external payable {}
 }
